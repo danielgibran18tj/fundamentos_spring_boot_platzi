ca7fa86c2ea9380bd65ba229a8a054d0e836b38c
danielgibran18tj <danielgibran18tj@hotmail.com>
2023-05-17 13:02:00 -0500

    haciendo consulta en jpql, usando la clase SORT

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -13,7 +13,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Sort;
 
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -51,26 +53,29 @@ public class FundamentosApplication implements CommandLineRunner {
 		LOGGER.info("Usuario con el metodo findByUserEmail"
 				+ userRepository.findByUserEmail("marisol@domain.com")
 				.orElseThrow(()-> new RuntimeException("No se encontro el usuario")));
+
+		userRepository.findAndSort("use", Sort.by("id").ascending())
+				.stream()
+				.forEach(user -> LOGGER.info("Usuario con metodo sort "+ user));
 	}
 
 	private void saveUsersInDataBase(){
 
-		User user1 = new User("John", "john@domain.com", LocalDate.of(2021, 3, 13));
+		User user1 = new User("userJohn", "john@domain.com", LocalDate.of(2021, 3, 13));
 		User user2 = new User("Julie", "julie@domain.com", LocalDate.of(2021, 12, 8));
 		User user3 = new User("Daniela", "daniela@domain.com", LocalDate.of(2021, 9, 8));
-		User user4 = new User("Marisol", "marisol@domain.com", LocalDate.of(2021, 6, 18));
-		User user5 = new User("Karen", "karen@domain.com", LocalDate.of(2021, 1, 1));
+		User user4 = new User("userMarisol", "marisol@domain.com", LocalDate.of(2021, 6, 18));
+		User user5 = new User("userKaren", "karen@domain.com", LocalDate.of(2021, 1, 1));
 		User user6 = new User("Carlos", "carlos@domain.com", LocalDate.of(2021, 7, 7));
 		User user7 = new User("Enrique", "enrique@domain.com", LocalDate.of(2021, 11, 12));
-		User user8 = new User("Luis", "luis@domain.com", LocalDate.of(2021, 2, 27));
+		User user8 = new User("userLuis", "luis@domain.com", LocalDate.of(2021, 2, 27));
 		User user9 = new User("Paola", "paola@domain.com", LocalDate.of(2021, 4, 10));
-		User user10 = new User("Damita", "damita@domain.com", LocalDate.of(2021, 6, 28));
+		User user10 = new User("userDamita", "damita@domain.com", LocalDate.of(2021, 6, 28));
 
 		List<User> list = Arrays.asList(user1, user2,user3,user4,user5,user6,user7,user8,user9,user10);
 		list.stream().forEach(userRepository::save);
 		//userRepository.saveAll(list);
 	}
-
 	private void ejemplosAnteriores(){
 		componentDependency.saludar();  	//utilizando dependencia como implementacion
 		myBean.print();
@@ -84,6 +89,5 @@ public class FundamentosApplication implements CommandLineRunner {
 		}catch (Exception e){
 			LOGGER.error("Esto es un error al dividir por cero " + e.getMessage());
 		}
-
 	}
 }
